d37d7898599b34a4cb637d9b85427c77d081b57d
QuentinChevaux <quentin.chevaux@gmail.com>
2021-11-10 14:43:25 +0100

    Post-It

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-let nouveau_post_it = new Post_It(800, 300, 200, 300, "blue", "Post It Vide", 1)
+let tableau_post_it = []
 
+document.getElementById("div_blue").addEventListener('click', ()=>{    
+        
+    tableau_post_it.push(new Post_It(800, 300, 200, 300, "blue", "Post It Bleu", tableau_post_it.length))
 
-nouveau_post_it.deplacement(1000, 400);
+    tableau_post_it[tableau_post_it.length-1].affichage()
 
-nouveau_post_it.redimension(500, 400);
+});
 
-nouveau_post_it.changer_texte("grnejgberkgerbgkerg");
+document.getElementById("div_green").addEventListener('click', () => {
+    
+    tableau_post_it.push(new Post_It(1000, 100, 200, 300, "green", "Post It Vert", tableau_post_it.length))
 
-nouveau_post_it.changer_couleur("red");
+    tableau_post_it[tableau_post_it.length-1].affichage()
 
-nouveau_post_it.affichage();
+});
+
+document.getElementById("div_pink").addEventListener('click', () => {
+
+    tableau_post_it.push(new Post_It(400, 600, 200, 300, "pink", "Post It Rose", tableau_post_it.length))
+
+    tableau_post_it[tableau_post_it.length-1].affichage()
+    
+
+});
--- post_it_class.js
+++ post_it_class.js
@@ -8,7 +8,7 @@ class Post_It {
     texte;
     id;
 
-    constructor(x, y, longeur, hauteur, couleur, texte, id) {
+    constructor(x, y, longeur, hauteur, couleur, texte, id, GrillePostit) {
 
         this.x = x
         this.y = y
@@ -29,6 +29,7 @@ class Post_It {
             nouveau_post_it = document.createElement("div")       
             document.body.appendChild(nouveau_post_it);
 
+        
         }        
 
         nouveau_post_it.id = "post_it" + this.id;
@@ -43,7 +44,66 @@ class Post_It {
 
         nouveau_post_it.style.backgroundColor = this.couleur;
 
-        nouveau_post_it.innerHTML = this.texte;       
+        nouveau_post_it.innerHTML = this.texte;
+        
+        nouveau_post_it.style.color = "white"
+
+        // MENU DES BOUTONS
+
+        let menu_post_it = document.createElement("div")
+        nouveau_post_it.appendChild(menu_post_it);
+
+        menu_post_it.className = "menu_post_it"
+
+            // BOUTON MOUVEMENT
+
+            let bouton_mouvement = document.createElement("div")
+            menu_post_it.appendChild(bouton_mouvement);
+
+            bouton_mouvement.className = "fas fa-arrows-alt"
+
+            // BOUTON REDIMENSION
+
+            let bouton_redimension = document.createElement("div")
+            menu_post_it.appendChild(bouton_redimension);
+
+            bouton_redimension.className = "fas fa-compress-alt"
+
+            // BOUTON CHANGER TEXTE
+
+            let bouton_changer_texte = document.createElement("div")
+            menu_post_it.appendChild(bouton_changer_texte);
+
+            bouton_changer_texte.className = "far fa-keyboard"
+
+            // BOUTON CHANGER COULEUR
+
+            let bouton_changer_couleur = document.createElement("div")
+            menu_post_it.appendChild(bouton_changer_couleur);
+
+            bouton_changer_couleur.className = "fas fa-fill-drip"
+
+            // BOUTON FRONT
+
+            let bouton_passer_premier_plan = document.createElement("div")
+            menu_post_it.appendChild(bouton_passer_premier_plan);
+
+            bouton_passer_premier_plan.className = "fas fa-arrow-up"
+            
+            // BOUTON BACK
+
+            let bouton_passer_second_plan = document.createElement("div")
+            menu_post_it.appendChild(bouton_passer_second_plan);
+
+            bouton_passer_second_plan.className = "fas fa-arrow-down"
+
+            // BOUTON SUPPRIMER
+
+            let bouton_supprimer = document.createElement("div")
+            menu_post_it.appendChild(bouton_supprimer);
+
+            bouton_supprimer.className = "fas fa-trash-alt" 
+
 
     }
 
